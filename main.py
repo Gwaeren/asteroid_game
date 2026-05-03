66c96595cdc11bce6afe0b71304e3b8bb1adc3c2
Gwaeren <gwaerenn@gmail.com>
2026-05-03 05:22:07 +0300

    Add shot.py. Update .gitignore, circleshape.py, constants.py, main.py, player.py. | Shots are added for the Space key.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 
 def main():
     pygame.init()
@@ -14,9 +16,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     
@@ -30,6 +34,11 @@ def main():
         for item in drawable:
             item.draw(screen)
         updatable.update(dt)
+        for item in asteroids:
+            if item.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
 
 
